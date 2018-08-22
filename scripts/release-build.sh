84a3d6cd8107a2b9173b8b24aa1b1ba56e138a42
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-08-22 12:52:30 -0700

    test --release-build

--- scripts/release-build.sh
+++ scripts/release-build.sh
@@ -2,5 +2,10 @@
 
 VERSION=`sed '1q;d' data/data.txt`
 MCVERSION=`sed '2q;d' data/data.txt`
+TITLEVERSION="Aeroteam-v${VERSION}"
 
 echo "${VERSION}"
+
+mkdir ${TITLEVERSION}/*
+
+mv ../* ${TITLEVERSION}/*
