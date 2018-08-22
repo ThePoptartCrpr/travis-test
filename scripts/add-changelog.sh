4e2699ffe68bbeee45484eaabf69022b1ff8491b
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-08-22 10:26:06 -0700

    fix i guess idk

--- scripts/add-changelog.sh
+++ scripts/add-changelog.sh
@@ -4,10 +4,7 @@ git config --global user.name "${GIT_NAME}"
 git config --global push.default simple
 
 msg="- $TRAVIS_BUILD_NUMBER: $TRAVIS_COMMIT_MESSAGE ($AUTHOR_NAME)"
-echo "$msg" >> $TRAVIS_BUILD_DIR/data/changelog.txt
+echo "$msg" >> data/changelog.txt
 
-cat > data/testfile.txt
-
-git add $TRAVIS_BUILD_DIR/data/changelog.txt
-git add data/testfile.txt
+git add data/changelog.txt
 git commit -m "Update changelog with $TRAVIS_BUILD_NUMBER [skip ci]"
