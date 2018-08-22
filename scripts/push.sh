a431499d0ae8b9d3deadc596b569f40262f4356f
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-08-22 10:17:52 -0700

    how about now

--- scripts/push.sh
+++ scripts/push.sh
@@ -18,7 +18,7 @@
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 # git remote set-url origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-git remote set-url origin https://${GH_TOKEN}@github.com/${GIT_NAME}/${TRAVIS_REPO_SLUG}.git
+git remote set-url origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 
 git push origin master
 
