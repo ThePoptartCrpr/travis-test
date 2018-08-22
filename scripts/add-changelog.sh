70108d49be5328711bc7daf319399af0361e1770
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-08-22 09:51:03 -0700

    please work

--- scripts/add-changelog.sh
+++ scripts/add-changelog.sh
@@ -4,8 +4,9 @@ MONTH=$(date +"%m")
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 git config --global push.default simple
-export GIT_TAG=V2.$YEAR-$MONTH.$TRAVIS_BUILD_NUMBER
-msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
+
+# export GIT_TAG=V2.$YEAR-$MONTH.$TRAVIS_BUILD_NUMBER
+msg="- $TRAVIS_BUILD_NUMBER: $TRAVIS_COMMIT_MESSAGE ($AUTHOR_NAME)"
 echo "$msg" >> $TRAVIS_BUILD_DIR/data/changelog.txt
 git add $TRAVIS_BUILD_DIR/data/changelog.txt
 git commit -m "Update build version file with $TRAVIS_BUILD_NUMBER"
